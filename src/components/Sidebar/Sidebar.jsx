8d871017359eaf2ea0f89b4f64bbade9bc393b4b
Shaian <shaianmokhles1380@gmail.com>
2024-04-05 14:52:43 +0330

    added suggestion, has redering bug

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,7 +12,8 @@ const Sidebar = () => {
             <label htmlFor="price">Price: {price}$</label>
             <input type="range" id="price" name="price" min={1} max={500} onChange={e => setPrice(e.target.value)} value={price} />
 
-            <select name="category" id="category" onChange={e => setCategories(e.target.value)} value={categories}>
+            <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select an option</label>
+            <select name="category" id="category" onChange={e => setCategories(e.target.value)} value={categories} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option value="">All</option>
                 <option value="women's clothing">Women`s Clothing</option>
                 <option value="men's clothing">Men`s Clothing</option>
